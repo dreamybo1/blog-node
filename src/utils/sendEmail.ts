8abaddfd3f35db5ae70820657e3d576a1020060d
Александр <dreamy@DreamyMac.local>
2025-11-08 13:47:22 +0500

    sendEmail fix

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,21 +1,18 @@
-import { createTransport } from "nodemailer";
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function sendEmail(to: string, subject: string, html: string) {
-  const transporter = createTransport({
-    host: "smtp.yandex.ru",
-    port: 465,
-    secure: true, // true for 465, false for other ports
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
-    },
-  });
-
-  const res = await transporter.sendMail({
-    from: `"DreamNet" <${process.env.SMTP_USER}>`,
-    to,
-    subject,
-    html,
-  });
-  console.log(res);
+  try {
+    const data = await resend.emails.send({
+      from: 'DreamNet', // купи домен за 1$
+      to: [to],
+      subject,
+      html,
+    });
+    console.log('Resend ✅', data);
+  } catch (error: any) {
+    console.error('Resend ❌', error);
+    throw error;
+  }
 }
